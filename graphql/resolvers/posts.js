3791c1923e2ff3404167aea644120a0c2a5028ef
Farrukh Jamal <52830299+farrukh806@users.noreply.github.com>
2021-06-24 17:02:27 +0530

    Like/unlike feature added

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -28,6 +28,9 @@ const postsResolvers = {
 	Mutation: {
 		createPost: async (_, { body }, context) => {
 			const user = checkAuth(context);
+			if (body.trim() === '') {
+				throw new UserInputError('Post must not be empty');
+			}
 			const newPost = new Post({
 				body,
 				username: user.username,
@@ -52,6 +55,32 @@ const postsResolvers = {
 					throw new Error(`Post not found`);
 				}
 			} catch (error) {
+				throw new Error(error);
+			}
+		},
+
+		likePost: async (_, { postId }, context) => {
+			const { username } = checkAuth(context);
+			try {
+				const post = await Post.findById(postId);
+				if (post) {
+					const alreadyLiked = await post.likes.findIndex(
+						(like) => like.username === username
+					);
+					console.log(alreadyLiked);
+					if (alreadyLiked > -1) {
+						post.likes.splice(alreadyLiked, 1);
+					} else {
+						await post.likes.push({ username });
+						console.log(username);
+					}
+					await post.save();
+					return post;
+				} else {
+					throw new Error(`Post not found`);
+				}
+			} catch (error) {
+				console.log(error);
 				throw new Error(error);
 			}
 		},
